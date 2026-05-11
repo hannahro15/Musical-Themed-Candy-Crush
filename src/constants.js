7d9641e5a1e4c7b3bd522af066ccd04c7163a96b
Hannah Olbrich <hannaholbrich@gmail.com>
2026-05-11 22:52:58 +0100

    refactor: change game board to 7x7 layout

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 // Centralised constants for the game
-export const BOARD_SIZE = 8;
+export const BOARD_SIZE = 7;
 export const SYMBOLS = ['🎵', '🎹', '🎻', '🎷', '🎺', '🥁'];
 export const INITIAL_LIVES = 5;
